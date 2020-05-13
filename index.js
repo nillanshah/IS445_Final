fe21d4822b8b8186ebb21a8cb33200f7dd3663e3
nillan <nillanshah@gmail.com>
2020-05-12 20:44:32 -0700

    Step2: cleaned up a lot of the if statements

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 function driver(){
+
+    var message_num1 = "";
+    var message_num2 = "";
+
+    var raw_num1 = document.getElementById("num1").value
+    var raw_num2 = document.getElementById("num2").value
+
+
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
 
-    console.log("num1:" + num1 + "\nnum2: " + num2);
+    if(isNaN(num1)){
+        message_num1 = "Number 1 input " + raw_num1 + " is not a valid  number";
+    } else if (num1 < 2 || num1 > 100){
+        message_num1 = "Number 1 input " + num1 + " is not within the range 2 and 100.";
+    }
+
+    if(isNaN(num2)){
+        message_num2 = "\nNumber 2 input " + raw_num2 + " is not a valid  number";
+    } else if (num2 < 2 || num2 > 100){
+        message_num2 = "\nNumber 2 input " + num2 + " is not within the range 2 and 100.";
+    }
+
+    document.getElementById("message").innerHTML = "<p style = 'color: red;'>" + message_num1 + "<br>" + message_num2 + "</p>";
+
 }
 
 document.getElementById("submit").addEventListener("click", driver);
