ad1d1f0bd0d979f99c8e22a69225cf23ad355e4c
nillan <nillanshah@gmail.com>
2020-05-12 20:08:27 -0700

    Step2: Adding parsing numbers from HTML using JS

--- index.js
+++ index.js
@@ -1,48 +1,8 @@
-function primeCheck(n){
-    if (n === 2){
-        return true;
-    } else {
-        for (var i = 2; i < n; i++){
-            if(n % i === 0){
-                return false;
-            }
-        }
-        return true;  
-    }
-}
-
-function validator(){
+function driver(){
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
 
-    if (num1 < 2 || num1 > 100 || num2 > 100 || num2 < 2 || isNaN(num1) || isNaN(num2)){
-        document.getElementById("message").innerHTML = "<p style = 'color: red;'> Invalid input, please try again </p>";
-    } else {
-
-        tempAr = []
-
-        if (num1 > num2){
-            for (i = num2; i <= num1; i++){
-                if (primeCheck(i)){
-                    tempAr.push(i);
-                }
-            }
-
-        } else if (num1 < num2){
-            for (i = num2; i >= num1; i--){
-                if (primeCheck(i)){
-                    tempAr.push(i);
-                }
-            }
-        }
-        tempAr.sort()
-
-        document.getElementById("message").innerHTML = "<p style = 'color: cyan;'>There are " + tempAr.length + " prime number(s)</p> <p style = 'color: red;'>" + tempAr + "</p>";
-
-    }
+    console.log("num1:" + num1 + "\nnum2: " + num2);
 }
 
-document.getElementById("submit").addEventListener("click", validator);
-
-
-
+document.getElementById("submit").addEventListener("click", driver);
